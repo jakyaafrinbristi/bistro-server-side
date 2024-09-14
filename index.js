ded18b4925b96cf94928116b8bb6cb579cbfd4e3
jakyaafrinbristi <jakyaafrinb@gmail.com>
2024-09-14 07:10:59 +0600

    user and menu data related get,post,delete,patrch data send

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express =require('express')
 const cors =require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 9000; 
 
 const app =express()
 const corsOptions ={
@@ -28,6 +29,104 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const menuCollection = client.db('bistroBoss').collection('menu');
+    const reviewCollection = client.db('bistroBoss').collection('reviews');
+
+    const cartsCollection = client.db('bistroBoss').collection('carts');
+
+    const usersCollection = client.db('bistroBoss').collection('users');
+
+
+
+    //jwt related 
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body;
+      const token=jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn:'365d'
+      });
+      res.send({token})
+    })
+
+
+
+    // users related api 
+
+    // get users related data in database
+    app.get('/users',async(req,res)=>{
+      const result=await usersCollection.find().toArray()
+      res.send(result)
+    })
+    // post users related data in database
+    app.post('/users',async(req,res)=>{
+      const user=req.body;
+      const query ={ email:user.email }
+      const existingUser =await usersCollection.findOne(query)
+      if(existingUser){
+        return res.send({message:'user already exist',insertedId:null})
+      }
+      const result=await usersCollection.insertOne(user)
+      res.send(result)
+    })
+    //patch related user data
+    app.patch('/users/admin/:id',async(req,res)=>{
+      const id =req.params.id;
+      const filter={_id:new ObjectId(id)}
+      const updatedDoc={
+        $set:{
+          role:'admin'
+        }
+      }
+      const result =await usersCollection.updateOne(filter,updatedDoc);
+      res.send(result)
+    })
+
+     // delete users related data in database
+     app.delete('/users/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query = {_id : new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query)
+      res.send(result)
+    })
+
+    //  menu related
+       // get all cards from mongo
+       app.get('/menu',async(req,res)=>{
+        const cursor =menuCollection.find();
+        const result =await cursor.toArray();
+        res.send(result);
+  
+      })
+       // get all cards from mongo
+       app.get('/reviews',async(req,res)=>{
+        const cursor =reviewCollection.find();
+        const result =await cursor.toArray();
+        res.send(result);
+  
+      })
+       // get all carts from mongo
+       app.get('/carts',async(req,res)=>{
+        const email=req.query.email
+        const query ={email : email}
+        const cursor =cartsCollection.find(query);
+        const result =await cursor.toArray();
+        res.send(result);
+  
+      })
+      // get delete
+      app.delete('/carts/:id',async(req,res)=>{
+        const id =req.params.id;
+        const query = {_id : new ObjectId(id)}
+        const result = await cartsCollection.deleteOne(query)
+        res.send(result)
+      })
+      //post data mongo
+      app.post('/carts',async(req,res)=>{
+        const cartItem=req.body;
+        const result=await cartsCollection.insertOne(cartItem)
+        res.send(result)
+      })
+
   
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
